156c0e461ad859c092114ff339b892db4f591b88
Lalainne Andaya <lcandaya2@student.apc.edu.ph>
2025-01-12 21:13:08 +0800

    Update gallery.js

--- home/js/gallery.js
+++ home/js/gallery.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
           { name: 'Profile', url: 'images/me.png' },
           { name: 'Me in Cebu', url: 'images/me in cebu.png' },
           { name: 'My niece and dog', url: 'images/my niece and dog.png' },
-          { name: 'My friends', url: 'images/friends.png' },
+          { name: 'My friends', url: 'images/friends4.png' },
           { name: 'My friends', url: 'images/friends2.png' },
           { name: 'My family', url: 'images/family.jpg' },
         ]
