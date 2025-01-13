bfc8a60ba123b4988760b4f00d5130455dd12ada
Lalainne Andaya <lcandaya2@student.apc.edu.ph>
2025-01-13 17:18:05 +0800

    Added gallery vue.js for gallery.html

--- home/js/gallery.js
+++ home/js/gallery.js
@@ -1,16 +1,16 @@
 const app = Vue.createApp({
-    data() {
-      return {
-        photos: [
-          { name: 'Profile', url: 'images/me.png' },
-          { name: 'Me in Cebu', url: 'images/me in cebu.png' },
-          { name: 'My niece and dog', url: 'images/my niece and dog.png' },
-          { name: 'My friends', url: 'images/friends4.png' },
-          { name: 'My friends', url: 'images/friends2.png' },
-          { name: 'My family', url: 'images/family.jpg' },
-        ]
-      };
-    }
-  });
+  data() {
+    return {
+      photos: [
+        { name: 'Profile', url: 'images/me.png' },
+        { name: 'Me in Cebu', url: 'images/me in cebu.png' },
+        { name: 'My niece and dog', url: 'images/my niece and dog.png' },
+        { name: 'My friends', url: 'images/friends4.png' },
+        { name: 'My friends', url: 'images/friends2.png' },
+        { name: 'My family', url: 'images/family.jpg' },
+      ]
+    };
+  }
+});
 
-  app.mount('#app');
+app.mount('#app');
