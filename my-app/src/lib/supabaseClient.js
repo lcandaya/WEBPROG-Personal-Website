5f01db99135f93de246d13e2457fa400b6d3ec5b
Lalainne Andaya <lcandaya2@student.apc.edu.ph>
2025-01-30 12:50:42 +0000

    npm install

--- my-app/src/lib/supabaseClient.js
+++
@@ -1,3 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient('https://iadxkwpnnvceoeymufvn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhZHhrd3BubnZjZW9leW11ZnZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyMzY3ODIsImV4cCI6MjA1MzgxMjc4Mn0.7UnwWOG0rVpev60SYq0xkE1jGqNRRhISjy_BljgjE-g')
